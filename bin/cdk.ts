520914df888050700521d31ffcfc5964daa6bf26
Daniel Isele <danielisele@gmail.com>
2025-08-08 09:09:46 -0500

    Initial commit

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,20 +1,24 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { CdkStack } from '../lib/cdk-stack'; // ✅ must match the export in lib
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+new CdkStack(app, 'CdkStack');
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+/* If you don't specify 'env', this stack will be environment-agnostic.
+ * Account/Region-dependent features and context lookups will not work,
+ * but a single synthesized template can be deployed anywhere.
+ *
+ * Uncomment the appropriate line below if you want to specialize this stack.
+ */
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// To specialize for the AWS Account and Region from the CLI config:
+// new CdkStack(app, 'CdkStack', {
+//   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// });
+
+// Or, to specify exact account and region:
+// new CdkStack(app, 'CdkStack', {
+//   env: { account: '123456789012', region: 'us-west-1' },
+// });
--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props); // ✅ Correct use of `this` starts here
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'HelloVpc', {
+      maxAzs: 2,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
+    const cluster = new ecs.Cluster(this, 'HelloCluster', {
+      vpc,
+    });
+
+    new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'HelloService', {
+      cluster,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      },
+      desiredCount: 2,
+      publicLoadBalancer: true,
+    });
+
+  } // ✅ Close constructor
+}   // ✅ Close class
